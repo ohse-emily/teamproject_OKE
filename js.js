d3d286f93bcc876839a273ccbfa670aca907e694
LS7875 <tmdgml7875@gmail.com>
2021-05-20 10:15:06 +0900

    comment location fixed

--- js.js
+++ js.js
@@ -1,19 +1,3 @@
-let a = '1.3423'
-let b = 2
-let c = 3
-let d = 4
+let a = ['aAaBdcDsCAaAAa'];
 
-if(a==1 || a==2 && b==2  ){
-    console.log('a')
-}
-console.log(a.length)
-
-
-let x = '1.1234'
-let y = +x;
-
-1.1234 
-1123.4 
-
-let ya=['a', '2']
-console.log(ya[1])
+console.log()
